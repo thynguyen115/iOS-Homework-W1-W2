e26226d771c93f21c37a860f35a07db963848025
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-09-26 14:55:44 -0400

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Thy Nguyen on 9/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    //@IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    //@IBOutlet weak var posterView: UIView!
+}
